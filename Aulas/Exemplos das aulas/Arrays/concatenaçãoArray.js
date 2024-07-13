36ea24416dc438aea8e658c92f4e89cbcf3bab23
Hiago Candido da Silva <contatohchiago@gmail.com>
2024-07-13 19:49:12 -0300

    concatenação de arrays

---
+++ Aulas/Exemplos das aulas/Arrays/concatenaçãoArray.js
@@ -0,0 +1,10 @@
+// Concatenação de arrays
+
+// Com o metodo concat()
+const a1 = [1, 2, 3]
+const a2 = [4, 5, 6]
+// const a3 = a1.concat(a2, [7, 8, 9], 'String')
+
+// Com spread syntax
+const a3 = [...a1, 'String', ...a2, ...[7, 8, 9]]
+console.log(a3)
