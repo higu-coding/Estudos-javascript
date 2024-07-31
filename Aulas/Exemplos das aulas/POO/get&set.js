1a8dbffade8ba27347d7f9c7a99bfeec4e4f963a
Hiago Candido da Silva <contatohchiago@gmail.com>
2024-07-31 18:09:54 -0300

    getters e setters em classes

---
+++ Aulas/Exemplos das aulas/POO/get&set.js
@@ -0,0 +1,37 @@
+// Getters e Setters em classes
+
+const _velocidade = Symbol('velocidade')
+
+class Carro {
+    constructor(nome) {
+        this.nome = nome
+        this[_velocidade] = 0
+    }
+
+    set velocidade(valor) {
+        console.log('Setter')
+        if (typeof valor !== 'number') return
+        if (valor >= 100 || valor <= 0) return
+        this[_velocidade] = valor
+    }
+
+    get velocidade() {
+        console.log('Getter')
+        return this[_velocidade]
+    }
+
+    acelerar() {
+        if (this[_velocidade] >= 100) return
+        this[_velocidade]++
+    }
+
+    freiar() {
+        if(this[_velocidade] <= 0) return
+        this[_velocidade]--
+    }
+}
+
+const c1 = new Carro('Fusca')
+
+c1.velocidade = 55
+console.log(c1.velocidade)
