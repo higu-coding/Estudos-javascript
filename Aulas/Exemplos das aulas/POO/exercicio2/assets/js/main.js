a2897d2bd105de5bc1bc028e7737642b6667950a
Hiago Candido da Silva <contatohchiago@gmail.com>
2024-08-07 17:53:39 -0300

    exercicio de formulario pt1

---
+++ Aulas/Exemplos das aulas/POO/exercicio2/assets/js/main.js
@@ -0,0 +1,27 @@
+class ValidaFormulario {
+    constructor() {
+        this.formulario = document.querySelector('.formulario')
+        this.eventos()
+    }
+
+    eventos() {
+        this.formulario.addEventListener('submit', e => {
+            this.handleSubmit(e)
+        })
+    }
+
+    handleSubmit(e) {
+        e.preventDefault()
+        const camposValidos = this.camposSaoValidos()
+    }
+
+    camposSaoValidos() {
+        let valid = true
+
+        for (let campo of this.formulario.querySelectorAll('.validar')){
+            
+        }
+    }
+}
+
+const valida = new ValidaFormulario()
