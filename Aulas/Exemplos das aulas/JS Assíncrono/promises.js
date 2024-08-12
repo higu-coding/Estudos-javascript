60fae85527b18f8a7122948e158c087b63a60672
Hiago Candido da Silva <contatohchiago@gmail.com>
2024-08-11 21:39:27 -0300

    promises

---
+++ Aulas/Exemplos das aulas/JS Assíncrono/promises.js
@@ -0,0 +1,32 @@
+function rand(min, max) {
+    min *= 1000
+    max *= 1000
+
+    return Math.floor(Math.random() * (max-min) + min)
+}
+
+function waiting(msg, tempo) {
+    return new Promise((resolve, reject) => {
+        if (typeof msg !== 'string') reject('BAD RESPONSE')
+        
+        setTimeout(() => {
+            resolve(msg)
+        }, tempo)
+    })
+}
+
+waiting('Conexão com o Banco de Dados', rand(1, 3)).then(resposta => {
+    console.log(resposta)
+    return waiting('Buscando dados da BASE', rand(1, 3))
+}).then(resposta => {
+    console.log(resposta)
+    return waiting(22222, rand(1, 3)) // ao colocar 22222 entre '' o codigo ira rodar normalmente
+}).then(resposta => {
+    console.log(resposta)
+}).then(() => {
+    console.log('Exibe dados na tela')
+}).catch(e => {
+    console.log('ERRO:', e)
+})
+
+console.log('Isto será exibido antes de qualquer promise.')
