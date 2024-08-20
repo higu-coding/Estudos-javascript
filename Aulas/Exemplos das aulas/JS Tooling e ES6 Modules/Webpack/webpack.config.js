ae8647e5e0f8f31b16eb7fc2af5813ad59fb89a5
Hiago Candido da Silva <contatohchiago@gmail.com>
2024-08-20 17:49:51 -0300

    definição do webpack

--- Aulas/Exemplos das aulas/JS Tooling e ES6 Modules/Webpack/webpack.config.js
+++ Aulas/Exemplos das aulas/JS Tooling e ES6 Modules/Webpack/webpack.config.js
@@ -1,3 +1,5 @@
+// O webpack é um empacotador de módulos estáticos para aplicações JavaScript modernas. Ao processar a aplicação o webpack gera um gráfico que mapeia cada módulo e suas dependências e gera um ou mais pacotes.
+
 const path = require('path'); // CommonJS
 
 module.exports = {
