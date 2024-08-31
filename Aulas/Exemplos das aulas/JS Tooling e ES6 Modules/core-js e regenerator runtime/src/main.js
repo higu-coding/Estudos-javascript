76ab1c1554741cf3d875baad8819e8d1b403e181
Hiago Candido da Silva <contatohchiago@gmail.com>
2024-08-30 22:59:19 -0300

    core-js e regenerator-runtime

---
+++ Aulas/Exemplos das aulas/JS Tooling e ES6 Modules/core-js e regenerator runtime/src/main.js
@@ -0,0 +1,9 @@
+// sempre que for rodar em navegadores mais antigos precisa instalar essas 2 dependencias
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
+// 
+import './assets/css/style.css'; 
+
+import execute from './modules/promises'
+
+execute()
---
+++ Aulas/Exemplos das aulas/JS Tooling e ES6 Modules/core-js e regenerator runtime/src/modules/promises.js
@@ -0,0 +1,16 @@
+
+
+function promise() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Sou a promise')
+            resolve()
+        }, 2000);
+    })
+}
+
+
+export default async function () {
+    await promise()
+    console.log('terminou')
+}
