4a9fb8b31b0c4a46e6c3ea16efe12ea7f4837f07
Hiago Candido da Silva <contatohchiago@gmail.com>
2024-10-17 18:23:52 -0300

    criando usuarios pt1 - deu um erro, vou arrumar quando chegar do trabalho

--- Aulas/Exemplos das aulas/JS + Node - Projeto Agenda (com Express + MongoDB)/Projeto Agenda/src/controllers/loginController.js
+++ Aulas/Exemplos das aulas/JS + Node - Projeto Agenda (com Express + MongoDB)/Projeto Agenda/src/controllers/loginController.js
@@ -1,3 +1,31 @@
+const Login = require('../models/LoginModel')
+
 exports.index = (req, res) => {
     res.render('login')
+}
+
+exports.register = async function(req, res){
+    try {
+        const login = new Login(req.body)
+        await login.register()
+    
+        if(login.errors.length > 0) {
+            req.flash('errors', login.errors)
+            req.session.save(function() {
+                return res.redirect('index') // na aula está 'back', mas nao estava funcionando do jeito certo
+            })
+            return
+        }
+
+        req.flash('success', 'Seu usuário foi criado com sucesso')
+            req.session.save(function() {
+                return res.redirect('index') // na aula está 'back', mas nao estava funcionando do jeito certo
+            })
+
+        return res.send(login.errors)
+    }catch(e){
+        console.log(e)
+        return res.render('404')
+    }
+    
 }
