ffa7056091729790cc1ba0f1a2cbedde6847023d
Hiago Candido da Silva <contatohchiago@gmail.com>
2024-10-21 18:05:01 -0300

    logando o usuario pt1

--- Aulas/Exemplos das aulas/JS + Node - Projeto Agenda (com Express + MongoDB)/Projeto Agenda/src/controllers/loginController.js
+++ Aulas/Exemplos das aulas/JS + Node - Projeto Agenda (com Express + MongoDB)/Projeto Agenda/src/controllers/loginController.js
@@ -25,3 +25,26 @@ exports.register = async function(req, res){
     }
     
 }
+
+
+exports.login = async function(req, res){
+    try {
+        const login = new Login(req.body)
+        await login.login()
+    
+        if(login.errors.length > 0) {
+            req.flash('errors', login.errors)
+            await req.session.save();
+            return res.redirect('index'); // estava tentando com 'back' porem não estava funcionando, e com index, funcionou
+            return
+        }
+
+        req.flash('success', 'Seu usuário foi criado com sucesso')
+            await req.session.save();
+            return res.redirect('index'); // estava tentando com 'back' porem não estava funcionando, e com index, funcionou
+    }catch(e){
+        console.log(e)
+        return res.render('404')
+    }
+    
+}
