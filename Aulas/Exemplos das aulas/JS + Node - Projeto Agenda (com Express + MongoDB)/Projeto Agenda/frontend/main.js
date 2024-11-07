36051bb47058c2f5b33d21114a378eb7de8f90cf
higu-coding <contatohchiago@gmail.com>
2024-11-07 20:09:25 -0300

    tentativa de validação do formulario do  front end pt1

--- Aulas/Exemplos das aulas/JS + Node - Projeto Agenda (com Express + MongoDB)/Projeto Agenda/frontend/main.js
+++ Aulas/Exemplos das aulas/JS + Node - Projeto Agenda (com Express + MongoDB)/Projeto Agenda/frontend/main.js
@@ -1,6 +1,7 @@
 import 'core-js/stable'
 import 'regenerator-runtime/runtime'
-//import './assets/css/style.css';
+import './assets/css/style.css';
+import './modules/Contato'
 
 import Login from './modules/Login'
 
